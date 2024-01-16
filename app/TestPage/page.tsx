687fd795aede5b12fd8827d33f6ed17ebc2812ad
Ensar Sulejmani <149506033+EnsarSulejmani@users.noreply.github.com>
2024-01-16 17:34:04 +0100

    Connected to Mongo DB
    Amin Login is temporarily admin registration for testing.

--- app/TestPage/page.tsx
+++ app/TestPage/page.tsx
@@ -4,7 +4,7 @@ import ButtonReject from "../components_global/buttons/ButtonReject";
 import ButtonNeutral from "../components_global/buttons/ButtonNeutral";
 import ButtonDisabeld from "../components_global/buttons/ButtonDisabeld";
 
-export default function TestPage() {
+export default async function TestPage() {
   return (
     <div className="flex flex-col gap-5 items-center justify-center h-[100vh]">
       <ButtonAction>Click Me</ButtonAction>
---
+++ models/admin.jsx
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.models.Admin || mongoose.model("Admin", userSchema);
