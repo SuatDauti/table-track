c44a71117f13fcbba9ae0ebce11e5a95983dbe1b
Ensar Sulejmani <149506033+EnsarSulejmani@users.noreply.github.com>
2024-01-27 01:38:18 +0100

    Products created at database

--- app/adminPanel/productsAdmin/addCategory/page.tsx
+++ app/adminPanel/productsAdmin/addCategory/page.tsx
@@ -8,9 +8,39 @@ import { useState } from "react";
 import ButtonAccept from "@/app/components_global/buttons/ButtonAccept";
 import ButtonNeutral from "@/app/components_global/buttons/ButtonNeutral";
 import DisplayProductsCategory from "@/app/components/adminProducts/Tables/displayProductsCategory";
+import { useRouter } from "next/navigation";
 
 export default function AddCategory() {
   const [Category, SetCategory] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/category", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          category: Category,
+        }),
+      });
+
+      if (res.ok) {
+        const form = e.target;
+        form.reset();
+        router.push("/adminPanel/productsAdmin");
+      } else {
+        console.log("User registration failed.");
+      }
+    } catch (error) {
+      console.log("Error during registration: ", error);
+    }
+  };
+
   return (
     <div className="relative flex flex-col items-center w-full gap-12 max-h-screen overflow-y-scroll">
       {/* Go back button */}
@@ -28,11 +58,11 @@ export default function AddCategory() {
 
       {/* Form */}
       <div className="w-full">
-        <form className="flex justify-around">
+        <form className="flex justify-around" onSubmit={handleSubmit}>
           <input
             onChange={(e) => SetCategory(e.target.value)}
             type="text"
-            placeholder="Position Name"
+            placeholder="Category Name"
             className="px-4 py-2 h-fit "
           ></input>
           <div className="flex flex-col gap-4">
--- app/components/adminProducts/productsDisplayTable.tsx
+++ app/components/adminProducts/productsDisplayTable.tsx
@@ -2,39 +2,60 @@ import ButtonAccept from "@/app/components_global/buttons/ButtonAccept";
 import ButtonNeutral from "@/app/components_global/buttons/ButtonNeutral";
 import Link from "next/link";
 
-export default function ProductsDisplayTable() {
+const getCategory = async function () {
+  try {
+    const res = await fetch("http://localhost:3000/api/category", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
+export default async function ProductsDisplayTable() {
+  const { category } = await getCategory();
   return (
-    <table className="text-white w-full text-right self-center  border-spacing-y-8 ">
-      <thead>
-        <tr className="border-b border-white">
-          <th>Category</th>
-          <th>Product Name</th>
-          <th>Product Price</th>
-          <th>In Stock</th>
-          <th>
-            <Link href={"/adminPanel/productsAdmin/1"} passHref>
-              <ButtonAccept className="border-none rounded-none shadow-none">
-                Add Product
-              </ButtonAccept>
-            </Link>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="border-b border-blackBody bg-white text-blackBody">
-          <td>Shank</td>
-          <td>Product Name</td>
-          <td>23.00$</td>
-          <td>25</td>
-          <td className=" text-end">
-            <Link href={"#"}>
-              <ButtonNeutral className="border-none rounded-none">
-                Edit
-              </ButtonNeutral>
-            </Link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      {category.map((t: any) => (
+        <table className="text-white w-full text-right self-center  border-spacing-y-8 ">
+          <thead>
+            <tr className="border-b border-white">
+              <th>{t.category}</th>
+              <th>Product Name</th>
+              <th>Product Price</th>
+              <th>In Stock</th>
+              <th>
+                <Link href={`/adminPanel/productsAdmin/${t.category}`} passHref>
+                  <ButtonAccept className="border-none rounded-none shadow-none">
+                    Add Product
+                  </ButtonAccept>
+                </Link>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border-b border-blackBody bg-white text-blackBody">
+              <td>Shank</td>
+              <td>Product Name</td>
+              <td>23.00$</td>
+              <td>25</td>
+              <td className=" text-end">
+                <Link href={"#"}>
+                  <ButtonNeutral className="border-none rounded-none">
+                    Edit
+                  </ButtonNeutral>
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ))}
+    </>
   );
 }
