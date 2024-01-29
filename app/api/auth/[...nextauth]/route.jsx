783f9772bc68936064a434bfc97c082e5522dbf7
Ensar Sulejmani <149506033+EnsarSulejmani@users.noreply.github.com>
2024-01-29 01:41:39 +0100

    Staff Sign In

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -1,5 +1,6 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import Admin from "@/models/admin";
+import Employee from "@/models/employee";
 
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -33,15 +34,39 @@ const authOptions = {
         }
       },
     }),
+
+    CredentialsProvider({
+      id: "staffCredentials",
+      name: "staffCredentials",
+      credentials: {},
+
+      async authorize(credentials) {
+        const { Pin } = credentials;
+        try {
+          await connectMongoDB();
+          const staff = await Employee.findOne({ Pin: Pin });
+
+          if (!staff) {
+            return null;
+          }
+
+          return staff;
+        } catch (error) {
+          console.log("Error: ", error);
+        }
+      },
+    }),
   ],
+
   session: {
     strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
-    signIn: "/adminLogin",
+    signIn: "/",
   },
 };
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
--- middleware.js
+++ middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/adminPanel/:path*"] };
+export const config = { matcher: ["/adminPanel/:path*", "/staffPanel/:path*"] };
